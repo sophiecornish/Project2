8743a2d025674f5bb52d0b5b5230218e93a302d5
Sophie Cornish <sophieahcornish@gmail.com>
2018-08-01 14:24:49 +0100

    carousel added

--- db/seeds.js
+++ db/seeds.js
@@ -11,12 +11,12 @@ Post.collection.drop();
 
 User
   .create([{
-    username: 'A',
+    username: 'Sarah Kemp',
     email: 'a@a.com',
     password: 'a',
     passwordConfirmation: 'a'
   }, {
-    username: 'B',
+    username: 'Sophie Lane',
     email: 'b@b.com',
     password: 'b',
     passwordConfirmation: 'b'
@@ -28,8 +28,8 @@ User
       title: 'Sustainable Practices',
       subtitle: 'We put sustainability at the core of everything we do.',
       author: users[0],
-      image: 'https://www.thereformation.com/media/W1siZiIsIjIwMTgvMDIvMjcvMTgvMDUvMDYvMDljMTBmMjMtNTg3NC00YTJjLWI2YzYtZjY3ODM1ZmMxY2VjLzktc2VydmljZWFuZGNvbW11bml0eS1hNmQ3OWNlODVkNzg5ZTliNGZlOTgzZDg4M2RhNTczZS5qcGciXV0/9-serviceandcommunity-a6d79ce85d789e9b4fe983d883da573e.jpg?sha=fbe59eb983bd7b16',
-      content: 'TEXT TEXT TEXT TEXT',
+      image: '../tshirts.jpg',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       readingTime: 8,
       likes: 293,
       comments: [
@@ -42,16 +42,16 @@ User
       subtitle: 'Going zero waste…',
       author: users[0],
       image: 'http://www.theecoedit.co.uk/wp-content/uploads/2018/02/3.jpg',
-      content: 'TEXT TEXT TEXT TEXT',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       readingTime: 6,
       likes: 201
     },
     {
-      title: 'At Harrods, Vetements Calls Out The Fashion Industry On Overproduction',
-      subtitle: 'Unveiled today, Vetements is taking over four Harrods windows to create awareness around the corporate overproduction the brand’s co-founder Guram Gvasalia says is destroying fashion and its surrounding world.',
+      title: 'Cotton',
+      subtitle: 'We do not think cotton is awesome',
       author: users[1],
-      image: 'http://img3.hungertv.com/wp-content/uploads/2018/02/VETEMENTS-SAKS--980x551.jpg',
-      content: 'TEXT TEXT TEXT TEXT',
+      image: 'https://www.thereformation.com/media/W1siZiIsIjIwMTgvMDcvMjcvMTgvMzcvNTAvMDA2NmM0ZGEtMjJlZC00OTE2LTkwOTEtZGE0NGM2MTZkNDgyLzYtY290dG9uLXBlc3RpY2lkZXMtMjM2ODI2NWI5ODllZTFmYTkzYzE0MjI4YTg2OGUyNTkuanBnIl1d/6-cotton-pesticides-2368265b989ee1fa93c14228a868e259.jpg?sha=ac85c52373856f5c',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       readingTime: 12,
       likes: 453
     },
@@ -60,7 +60,7 @@ User
       subtitle: 'We put sustainability at the core of everything we do.',
       author: users[0],
       image: 'https://www.thereformation.com/media/W1siZiIsIjIwMTgvMDIvMjcvMTgvMDUvMDYvMDljMTBmMjMtNTg3NC00YTJjLWI2YzYtZjY3ODM1ZmMxY2VjLzktc2VydmljZWFuZGNvbW11bml0eS1hNmQ3OWNlODVkNzg5ZTliNGZlOTgzZDg4M2RhNTczZS5qcGciXV0/9-serviceandcommunity-a6d79ce85d789e9b4fe983d883da573e.jpg?sha=fbe59eb983bd7b16',
-      content: 'TEXT TEXT TEXT TEXT',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       readingTime: 8,
       likes: 293,
       comments: [
@@ -69,20 +69,47 @@ User
       ]
     },
     {
-      title: '30 Days to Zero-Waste, a guide to more conscious living',
+      title: 'Offsets',
       subtitle: 'Going zero waste…',
       author: users[0],
-      image: 'http://www.theecoedit.co.uk/wp-content/uploads/2018/02/3.jpg',
-      content: 'TEXT TEXT TEXT TEXT',
+      image: 'https://www.thereformation.com/media/W1siZiIsIjIwMTgvMDMvMDgvMTkvMTMvMDIvZTQwZGIwNTQtN2NhNy00MjU1LWE4ZDEtMjgzYmY1MTFiMzEyLzEuanBnIl1d/1.jpg?sha=e361ec484553ad4f',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       readingTime: 6,
       likes: 201
     },
     {
-      title: 'At Harrods, Vetements Calls Out The Fashion Industry On Overproduction',
-      subtitle: 'Unveiled today, Vetements is taking over four Harrods windows to create awareness around the corporate overproduction the brand’s co-founder Guram Gvasalia says is destroying fashion and its surrounding world.',
+      title: '30 Days to Zero-Waste, a guide to more conscious living',
+      subtitle: 'Going zero waste…',
+      author: users[0],
+      image: 'http://www.theecoedit.co.uk/wp-content/uploads/2018/02/3.jpg',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      readingTime: 6,
+      likes: 201
+    },
+    {
+      title: 'Protect the Earth',
+      subtitle: 'Going zero waste…',
+      author: users[0],
+      image: 'https://www.thereformation.com/media/W1siZiIsIjIwMTgvMDcvMjcvMTcvNDkvMjgvODBkM2QzZDYtMjA4Yy00YzlmLWFlMmMtMjFhNmRjNDEyMzlmLzMtY2xpbWF0ZS1jaGFuZ2UtYjBmZjMwY2U5ODdkNjg3YTQ0YjVhYTJjOWNmNGI2ODMuanBnIl1d/3-climate-change-b0ff30ce987d687a44b5aa2c9cf4b683.jpg?sha=7574bda580c0c61c',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      readingTime: 6,
+      likes: 201
+    },
+    {
+      title: 'Sustainability Report',
+      subtitle: 'We think companies should be accountable for more than just profits.',
+      author: users[0],
+      image: 'https://www.thereformation.com/media/W1siZiIsIjIwMTgvMDYvMjgvMTkvMjQvMjUvYzRkNmM4MmUtNjIwMi00YmUwLWE3ZjAtZWI1OTM5Y2UxNTI0L1JlZm9ybWF0aW9uLU5ldXRyYWwuanBnIl1d/Reformation-Neutral.jpg?sha=19e0ce6a8608be49',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      readingTime: 12,
+      likes: 325
+    },
+    {
+      title: 'Our Favourite Sustainable Brands',
+      subtitle: 'The number one sustainable option is being naked, here\'s number 2',
       author: users[1],
-      image: 'http://img3.hungertv.com/wp-content/uploads/2018/02/VETEMENTS-SAKS--980x551.jpg',
-      content: 'TEXT TEXT TEXT TEXT',
+      image: 'https://www.thereformation.com/media/W1siZiIsIjIwMTgvMDMvMTYvMTQvMDgvMTgvYTk1ZWEyZTEtMDI4MS00OWJjLWJlYzctYjY5OWNjYmRiODQ4LzIuanBnIl1d/2.jpg?sha=a414b65d489a6f1c',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       readingTime: 12,
       likes: 453
     }])
@@ -90,44 +117,3 @@ User
       .catch(err => console.log(err))
       .finally(() => mongoose.connection.close());
   });
-
-// Post
-//   .create([
-//     {
-//       title: 'Sustainable Practices',
-//       subtitle: 'We put sustainability at the core of everything we do.',
-//       authorName: 'Reformation',
-//       image: 'https://www.thereformation.com/media/W1siZiIsIjIwMTgvMDIvMjcvMTgvMDUvMDYvMDljMTBmMjMtNTg3NC00YTJjLWI2YzYtZjY3ODM1ZmMxY2VjLzktc2VydmljZWFuZGNvbW11bml0eS1hNmQ3OWNlODVkNzg5ZTliNGZlOTgzZDg4M2RhNTczZS5qcGciXV0/9-serviceandcommunity-a6d79ce85d789e9b4fe983d883da573e.jpg?sha=fbe59eb983bd7b16',
-//       content: 'TEXT TEXT TEXT TEXT',
-//       date: '12th August',
-//       readingTime: 8,
-//       likes: 293,
-//       comments: [
-//         {name: 'Sophie', content: 'Wow I never knew'},
-//         {name: 'Laura', content: 'Thanks for sharing. Check out my blog!'}
-//       ]
-//     },
-//     {
-//       title: '30 Days to Zero-Waste, a guide to more conscious living',
-//       subtitle: 'Going zero waste…',
-//       authorName: 'The Eco Edit',
-//       image: 'http://www.theecoedit.co.uk/wp-content/uploads/2018/02/3.jpg',
-//       content: 'TEXT TEXT TEXT TEXT',
-//       date: '2nd September',
-//       readingTime: 6,
-//       likes: 201
-//     },
-//     {
-//       title: 'At Harrods, Vetements Calls Out The Fashion Industry On Overproduction',
-//       subtitle: 'Unveiled today, Vetements is taking over four Harrods windows to create awareness around the corporate overproduction the brand’s co-founder Guram Gvasalia says is destroying fashion and its surrounding world.',
-//       authorName: 'Vogue',
-//       image: 'http://img3.hungertv.com/wp-content/uploads/2018/02/VETEMENTS-SAKS--980x551.jpg',
-//       content: 'TEXT TEXT TEXT TEXT',
-//       date: '8th July',
-//       readingTime: 12,
-//       likes: 453
-//     }
-//   ])
-//   .then(posts => console.log(`created ${posts.length} posts`))
-//   .catch(err => console.log(err))
-//   .finally(() => mongoose.connection.close());
